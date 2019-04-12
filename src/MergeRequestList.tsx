c116e700a5567ff0588cfd4744b37a0d0aa493d9
Tom Berriot <tom51100@live.fr>
2019-04-12 19:01:09 +0200

    Add ts types for fetched MergeRequest
    Fix : img tag doesn't accept null :/
    
    
    Remove file types.ts for specific file for MR type
    
    
    Rename TS Interface
    
    
    Rename TS interface

--- src/MergeRequestList.tsx
+++ src/MergeRequestList.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import MergeRequestItem from './MergeRequestItem';
-import { MergeRequestType } from './types';
+import { MergeRequestType } from './types/MergeRequest';
 
 interface Props {
     mergeRequests: MergeRequestType[];
@@ -18,7 +18,7 @@ class MergeRequestList extends Component<Props, State> {
   public render() {
     let { mergeRequests } = this.props ;
 
-    const listItems = mergeRequests.map((mergeRequest) =>
+    const listItems = mergeRequests.map((mergeRequest: MergeRequestType) =>
       <MergeRequestItem key={mergeRequest.id} mergeRequest={mergeRequest} />
     );
 
