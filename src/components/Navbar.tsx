6d120080fb23fc7d5d4008c643fda9e4cfe57f47
Tom Berriot <tom51100@live.fr>
2019-04-15 23:40:43 +0200

    Add Fontawesome

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 interface Props {
 }
@@ -15,7 +17,9 @@ class Navbar extends Component<Props, State> {
                 <h1 className="font-semibold text-2xl tracking-tight">Gitlab Reviewer</h1>
             </div>
             <div>
-                <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Download</a>
+                <a href="https://github.com/TheGrowingPlant/gitlab-reviewer" target="_blank">
+                    <FontAwesomeIcon icon={faGithub} size="2x" color="white"/>
+                </a>
             </div>
         </nav>
     );
