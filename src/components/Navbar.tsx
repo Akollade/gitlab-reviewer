8042617d5e9348b06843efa633f13f2ebdc0dcaf
Tom Berriot <tom51100@live.fr>
2019-04-17 23:41:33 +0200

    Fix GitLab word and add mention "Fork me on [Github Logo]

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,13 @@ class Navbar extends Component {
         <nav className="flex items-center justify-between flex-wrap bg-blue-darkest p-6">
             <div className="text-white mr-6">
                 <a className="text-white no-underline" href="" onClick={() => window.location.reload()}>
-                    <h1 className="font-semibold text-3xl tracking-tight">Gitlab Reviewer</h1>
+                    <h1 className="font-semibold text-3xl tracking-tight">GitLab Reviewer</h1>
                 </a>
             </div>
             <div>
-                <a href="https://github.com/TheGrowingPlant/gitlab-reviewer" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} size="2x" color="white"/>
+                <a className="text-white hover:text-grey no-underline flex " href="https://github.com/TheGrowingPlant/gitlab-reviewer" target="_blank" rel="noopener noreferrer">
+                    <p className="pr-2 pt-1">Fork me on</p>
+                    <FontAwesomeIcon icon={faGithub} size="2x" className="text-white hover:text-grey"/>
                 </a>
             </div>
         </nav>
