96c5ff9570cba86f4c7f700d780696045fd552d6
Floran Brutel <contact@floran.fr>
2019-04-23 18:31:47 +0200

    Use prevprops to update favicon only when the number of MR change

--- src/components/MergeRequest/MergeRequestList.tsx
+++ src/components/MergeRequest/MergeRequestList.tsx
@@ -30,8 +30,10 @@ class MergeRequestList extends Component<Props> {
     });
   }
 
-  public componentDidUpdate() {
-    this.favicon.badge(this.props.mergeRequests.length);
+  public componentDidUpdate(prevProps: Props) {
+    if (this.props.mergeRequests.length !== prevProps.mergeRequests.length) {
+      this.favicon.badge(this.props.mergeRequests.length);
+    }
   }
 
 
