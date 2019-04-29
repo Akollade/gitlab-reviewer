f52ec337ca8b3a5f4d6e0c9cbff93142971c8e44
Tom Berriot <tom51100@live.fr>
2019-04-29 18:33:32 +0200

    simpler code

--- src/components/FavicoMergeRequestsCounter.tsx
+++ src/components/FavicoMergeRequestsCounter.tsx
@@ -4,6 +4,13 @@ import { Project } from 'types/FormattedTypes';
 // see https://github.com/ejci/favico.js/issues/126
 const Favico = require('favico.js');
 
+function getMergeRequestsCount(projects: Project[]): number {
+  return projects.reduce<number>(
+    (counter: number, project: Project): number => counter + project.mergeRequests.length,
+    0
+  );
+}
+
 interface Props {
   projects: Project[];
 }
@@ -11,21 +18,14 @@ interface Props {
 class FavicoMergeRequestsCounter extends Component<Props> {
   private favicon: favicojs.Favico = new Favico({ animation: 'fade' });
 
-  private static getMergeRequestsCount(projects: Project[]): number {
-    return projects.reduce<number>(
-      (counter: number, project: Project): number => counter + project.mergeRequests.length,
-      0
-    );
-  }
-
   public componentDidMount() {
-    const mergeRequestsLength = FavicoMergeRequestsCounter.getMergeRequestsCount(this.props.projects);
+    const mergeRequestsLength = getMergeRequestsCount(this.props.projects);
     this.favicon.badge(mergeRequestsLength);
   }
 
   public componentDidUpdate({ projects: prevProjects }: Props) {
-    const oldMergeRequestsLength = FavicoMergeRequestsCounter.getMergeRequestsCount(prevProjects);
-    const newMergeRequestsLength = FavicoMergeRequestsCounter.getMergeRequestsCount(this.props.projects);
+    const oldMergeRequestsLength = getMergeRequestsCount(prevProjects);
+    const newMergeRequestsLength = getMergeRequestsCount(this.props.projects);
 
     if (newMergeRequestsLength !== oldMergeRequestsLength) {
       this.favicon.badge(newMergeRequestsLength);
@@ -40,5 +40,7 @@ class FavicoMergeRequestsCounter extends Component<Props> {
     return null;
   }
 }
+
+
 
 export default FavicoMergeRequestsCounter;
