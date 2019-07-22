81c85df7acf619b83ed9aa68cf8d1f04a9dbab33
Tom Berriot <tom51100@live.fr>
2019-07-22 23:39:09 +0200

    Display user personal votes (thumbup/down/empty) on each mr

--- src/components/MergeRequest/MergeRequestItem.tsx
+++ src/components/MergeRequest/MergeRequestItem.tsx
@@ -3,6 +3,9 @@ import Pill from 'components/Pill';
 import React, { Component } from 'react';
 import Emojify from 'react-emojione';
 import { MergeRequest } from 'types/FormattedTypes';
+import { UserContext } from 'components/UserProvider';
+import { User } from 'types/GitLabTypes';
+import { UpvoteIcon, DownvoteIcon, QuestionIcon } from 'components/Icons';
 
 interface Props {
   mergeRequest: MergeRequest;
@@ -17,34 +20,47 @@ class MergeRequestItem extends Component<Props> {
     const { mergeRequest } = this.props;
 
     return (
-      <tr>
-        <td>
-          <a
-            className="text-xl no-underline text-black"
-            href={mergeRequest.web_url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Emojify>{mergeRequest.title}</Emojify>
-          </a>
-        </td>
-        <td className="w-32 flex justify-center">
-          <img
-            src={mergeRequest.author.avatar_url}
-            alt={mergeRequest.author.name}
-            className="border-2 border-gray-500 h-10 w-10"
-          />
-        </td>
-        <td className="w-32 text-center">
-          <Pill text={mergeRequest.upvotes} type={mergeRequest.upvotes > 0 ? 'success' : 'disable'} />
-        </td>
-        <td className="w-32 text-center">
-          <Pill text={mergeRequest.downvotes} type={mergeRequest.downvotes > 0 ? 'danger' : 'disable'} />
-        </td>
-        <td className="w-32 text-center">
-          <PipelineStatusButton pipeline={mergeRequest.pipeline}/>
-        </td>
-      </tr>
+      <UserContext.Consumer>
+        {(user: User) => {
+
+          const haveIUpVoted = mergeRequest.upvoters.find((upvoter) => upvoter.id === user.id) ? true : false;
+          const haveIDownVoted = mergeRequest.downvoters.find((downvoter) => downvoter.id === user.id) ? true : false;
+
+          return (
+            <tr>
+              <td>
+                <a
+                  className="text-xl no-underline text-black"
+                  href={mergeRequest.web_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Emojify>{mergeRequest.title}</Emojify>
+                </a>
+              </td>
+              <td className="w-32 flex justify-center">
+                <img
+                  src={mergeRequest.author.avatar_url}
+                  alt={mergeRequest.author.name}
+                  className="border-2 border-gray-500 h-10 w-10"
+                />
+              </td>
+              <td className="w-32 text-center">
+                {haveIUpVoted ? <UpvoteIcon size={'lg'} /> : (haveIDownVoted ? <DownvoteIcon size={'lg'} /> : <QuestionIcon size={'lg'} />)}
+              </td>
+              <td className="w-32 text-center">
+                <Pill text={mergeRequest.upvotes} type={mergeRequest.upvotes > 0 ? 'success' : 'disable'} />
+              </td>
+              <td className="w-32 text-center">
+                <Pill text={mergeRequest.downvotes} type={mergeRequest.downvotes > 0 ? 'danger' : 'disable'} />
+              </td>
+              <td className="w-32 text-center">
+                <PipelineStatusButton pipeline={mergeRequest.pipeline} />
+              </td>
+            </tr>
+          )
+        }}
+      </UserContext.Consumer>
     );
   }
 }
