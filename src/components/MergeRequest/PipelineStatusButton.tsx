06bd66ffbedd5d191056659e81e17feeb1b2a9cb
Nicolas Hart <contact@nclshart.net>
2019-11-05 22:31:47 +0100

    reduce line height on CI button

--- src/components/MergeRequest/PipelineStatusButton.tsx
+++ src/components/MergeRequest/PipelineStatusButton.tsx
@@ -35,7 +35,7 @@ class PipelineStatusButton extends Component<Props> {
 
     return (
       <a
-        className={'inline-block text-white p-2 rounded shadow font-bold w-20 ' + this.getStyleFromStatus()}
+        className={'inline-block text-white px-2 py-3 rounded shadow font-bold w-20 leading-none ' + this.getStyleFromStatus()}
         target="_blank"
         rel="noopener noreferrer"
         href={pipeline.web_url}
